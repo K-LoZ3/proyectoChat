2f19e15cd3023e2cbf54b3f353443bf4d4486d8e
@K.LoZ3 <carlos193zambra@gmail.com>
2025-07-27 14:02:24 -0500

    Empiezo a usar chi.

--- main.go
+++ main.go
@@ -1,39 +1,40 @@
 //El codigo de chat es del ejemplo de gorilla/websocket
 //estare modificando el protecto a partir de aqui.
 
-
-
 package main
 
 import (
-	"flag"
+  "time"
 	"log"
 	"net/http"
+	
+	"github.com/go-chi/chi/v5"
 )
 
 func serveHome(w http.ResponseWriter, r *http.Request) {
 	log.Println(r.URL)
-	if r.URL.Path != "/" {
-		http.Error(w, "Not found", http.StatusNotFound)
-		return
-	}
-	if r.Method != http.MethodGet {
-		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
-		return
-	}
+
 	http.ServeFile(w, r, "./home.html")
 }
 
 func main() {
   addr := "10.254.97.246:8080"
   
-	flag.Parse()
+  r :=  chi.NewRouter()
+  
 	hub := newHub()
 	go hub.run()
-	http.HandleFunc("/", serveHome)
-	http.HandleFunc("/ws", func(w http.ResponseWriter, r *http.Request) {
-		serveWs(hub, w, r)
+	
+	r.Route("/", func(r chi.Router) {
+	  r.Get("/", serveHome)
+	  
+  	r.Get("/ws", func(w http.ResponseWriter, r *http.Request) {
+  		serveWs(hub, w, r)
+  		
+  	})
+  	
 	})
+	
 	
 	server := http.Server{
     Addr: addr,
